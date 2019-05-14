93d15b57a8a0b61080d81cb504ba98f2c3164044
Bumblebee <arthurmakumbi@Bumblebee-Arthur-Air.local>
2019-05-13 22:55:34 -0400

    running calibrations and saving parameters to files

--- src/augCalibDisplay.cpp
+++ src/augCalibDisplay.cpp
@@ -1,14 +1,11 @@
 /*
 	Arthur and Shafat
 	S19
-	Simple example of video capture and manipulation
+	Simple example of Calibration and Augmented Reality
 	Based on OpenCV tutorials
 
-	Compile command (macos)
-	clang++ -o vid -I /opt/local/include vidDisplay.cpp -L /opt/local/lib -lopencv_core -lopencv_highgui -lopencv_video -lopencv_videoio
-
 	use the makefiles provided
-	make vid
+	make aug
 */
 
 #include <cstdio>
@@ -29,7 +26,7 @@ using namespace std;
 vector<Point3f> generatePointSet(Size boardSize){
 	vector<Point3f> pointSet;
 	for (int i = 0; i < boardSize.height; i++){
-		for (int j = 0; j < boardSize.height; j++){
+		for (int j = 0; j < boardSize.width; j++){
 			Point3f point(j, -i, 0);
 			pointSet.push_back(point);
 		}
@@ -64,16 +61,22 @@ int main(int argc, char *argv[]) {
 	// matrices to hold multiple image outputs, etc
 	Mat frame, frameGray;
 	vector<Point2f> corner_set;
-	vector<Point3f>point_set;
+	vector<Point3f> point_set;
 	vector<vector<Point2f>> corner_list;
 	vector<vector<Point3f>> point_list;
 	int columns, rows, chessBoardFlags;
 	bool targetFound;
 
+	Mat cameraMatrix = Mat::eye(3, 3, CV_64FC1);
+	cameraMatrix.at<float>(0,2) = frame.cols/2;
+    cameraMatrix.at<float>(1,2) = frame.rows/2;
+
+	Mat distCoeffs = Mat::zeros(8, 1, CV_64F);
 
 	for(;!quit;) {
 		
-		*capdev >> frame; // get a new frame from the camera, treat as a stream
+		// *capdev >> frame; // get a new frame from the camera, treat as a stream
+		frame = imread("../data/checkerboard.png");
 
 		if( frame.empty() ) {
 		  printf("frame is empty\n");
@@ -102,7 +105,6 @@ int main(int argc, char *argv[]) {
 		}
 
 
-
 		// display the video frame in the window
 		imshow("Video", frame);
 
@@ -116,13 +118,37 @@ int main(int argc, char *argv[]) {
 				point_list.push_back(generatePointSet(boardSize));
 				// save current calibration image
 				imwrite("../data/calib" +to_string(corner_list.size())+ ".png", frame);
+				
+				// writing to files
+				FileStorage fs1("camParameters.xml", FileStorage::WRITE);
+				fs1 << "cameraMatrix" << cameraMatrix;
+
+				FileStorage fs2("distCoeffs.xml", FileStorage::WRITE);
+				fs2 << "distCoeffs" << distCoeffs;
+
 				break;
 
-			// c calibrate
+			// c calibrate If the user has selected enough calibration frames
+				// --require at least 5--then let the user run a calibration
 			case 'c':
-				if(){
-					
+				if(corner_list.size() >= 1){
+					printf("Calibrating...\n");
+					// cout << corner_list.size() << endl;
+					// cout << point_list.size() << endl;
+					// cout << corner_list[0].size() << endl;
+					// cout << point_list[0].size() << endl;
+
+					vector<Mat> rotationVecs, translationVecs;
+					int flag = CALIB_FIX_ASPECT_RATIO;
+					double reProjectionError = calibrateCamera(point_list, corner_list, frame.size(), cameraMatrix, distCoeffs, rotationVecs, translationVecs, flag);
+
+					cout << "reProjectionError : " << reProjectionError << endl;
+					cout << "cameraMatrix \n" << cameraMatrix << endl;
+					cout << "distCoeffs \n" << distCoeffs << endl;
+					cout << "frame.cols/2 \n" << frame.cols/2 << "  frame.rows/2\n" << frame.rows/2 << endl;
+
 				}
+				break;
 
 			// q quits the program
 			case 'q':
@@ -130,7 +156,7 @@ int main(int argc, char *argv[]) {
 				break;
 
 			// capture a photo if the user hits c
-			case 'c': 
+			case 'p': 
 				sprintf(buffer, "%s.%03d.png", label, frameid++);
 				imwrite(buffer, frame, pars);
 				printf("Image written: %s\n", buffer);
